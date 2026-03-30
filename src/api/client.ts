3075c5c9552c98e0b66ba35a629d7f6537d1ac5a
Rktmmnrn <rfanomezaniavo@gmail.com>
2026-03-30 17:11:15 +0300

    feat: amelioration client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:8000/api/',
-  headers: { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json' // pour le debug
+  },
   timeout: 10000,
 });
 
